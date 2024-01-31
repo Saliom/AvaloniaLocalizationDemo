5d33b0dd6225fd8870a3f365f456084ec17d8881
Saliom <tedjsaliom@gmail.com>
2024-01-31 12:24:38 +0100

    Added UI controls, bindings and events to do the Language selection changes

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using System.Globalization;
 using System.ComponentModel;
 
 namespace AvaloniaLocalizationDemo
@@ -6,12 +7,45 @@ namespace AvaloniaLocalizationDemo
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public new event PropertyChangedEventHandler PropertyChanged;
+        public string HelloTxt => Lang.Resources.Hello;
+        public string CurrentCultureTxt => Lang.Resources.Culture != null ? Lang.Resources.Culture.Name : "en-EN";
 
         public MainWindow()
         {
             InitializeComponent();
 
             this.DataContext = this;
+        }
+
+        private void ComboBox_SelectionChanged(object? sender, Avalonia.Controls.SelectionChangedEventArgs e)
+        {
+            var comboBox = sender! as Avalonia.Controls.ComboBox;
+
+            switch (comboBox!.SelectedIndex)
+            {
+                case 0:
+                    Lang.Resources.Culture = new CultureInfo("fr-FR");
+                    break;
+                case 1:
+                    Lang.Resources.Culture = new CultureInfo("en-EN");
+                    break;
+                case 2:
+                    Lang.Resources.Culture = new CultureInfo("zh-CN");
+                    break;
+                case 3:
+                    Lang.Resources.Culture = new CultureInfo("ja-JP");
+                    break;
+                case 4:
+                    Lang.Resources.Culture = new CultureInfo("fil-PH");
+                    break;
+                default:
+                    Lang.Resources.Culture = new CultureInfo("en-EN");
+                    break;
+            }
+
+            OnPropertyChanged(nameof(HelloTxt));
+            OnPropertyChanged(nameof(CurrentCultureTxt));
+
         }
 
         protected virtual void OnPropertyChanged(string propertyName)
