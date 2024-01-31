1856f405cd17ef6dc418f28e0100f51d07678af4
Saliom <tedjsaliom@gmail.com>
2024-01-31 12:09:48 +0100

    Added INotifyPropertyChanged implementation to MainWindow

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,12 +1,22 @@
 using Avalonia.Controls;
+using System.ComponentModel;
 
 namespace AvaloniaLocalizationDemo
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public new event PropertyChangedEventHandler PropertyChanged;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.DataContext = this;
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
